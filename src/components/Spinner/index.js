b6ca1bf189118a78e96a27cc647a59119d03617e
Ben Mildren <mildrenben@gmail.com>
2018-11-10 15:44:00 +0000

    a

--- src/components/Spinner/index.js
+++ src/components/Spinner/index.js
@@ -9,51 +9,51 @@ const Text = styled.h3`
 `
 
 class Spinner extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      showing: '',
-      id: 0
-    }
-  }
-  getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
-  componentDidMount() {
-    const { items, pick } = this.props
-    const firstTimer = setInterval(() => this.setState({
-      showing: items[this.getRandomInt(items.length)],
-    }), 70)
-    setTimeout(() => clearInterval(firstTimer), 3500)
+	constructor(props) {
+		super(props)
+		this.state = {
+			showing: '',
+			id: 0
+		}
+	}
+	getRandomInt(max) {
+		return Math.floor(Math.random() * Math.floor(max))
+	}
+	componentDidMount() {
+		const { items, pick } = this.props
+		const firstTimer = setInterval(() => this.setState({
+			showing: items[this.getRandomInt(items.length)],
+		}), 70)
+		setTimeout(() => clearInterval(firstTimer), 3500)
     
-    let secondTimer
-    setTimeout(
-      () => {
-        secondTimer = setInterval(() => this.setState({
-          showing: items[this.getRandomInt(items.length)],
-        }), 200)
-      }, 3500
-      )
-    setTimeout(() => clearInterval(secondTimer), 7000)
+		let secondTimer
+		setTimeout(
+			() => {
+				secondTimer = setInterval(() => this.setState({
+					showing: items[this.getRandomInt(items.length)],
+				}), 200)
+			}, 3500
+		)
+		setTimeout(() => clearInterval(secondTimer), 7000)
 
-    let thirdTimer
-    setTimeout(
-      () => {
-        thirdTimer = setInterval(() => this.setState({
-          showing: items[this.getRandomInt(items.length)],
-        }), 400)
-      }, 7000
-      )
-    setTimeout(() => clearInterval(thirdTimer), 10000)
-    setTimeout(() => {
-      document.querySelector('#pick').classList.add('tada')
-    }, 11000)
-  }
-  render() {
-    return (
-      <Text id='pick'>{this.state.showing}</Text>
-    )
-  }
+		let thirdTimer
+		setTimeout(
+			() => {
+				thirdTimer = setInterval(() => this.setState({
+					showing: items[this.getRandomInt(items.length)],
+				}), 400)
+			}, 7000
+		)
+		setTimeout(() => clearInterval(thirdTimer), 10000)
+		setTimeout(() => {
+			document.querySelector('#pick').classList.add('tada')
+		}, 11000)
+	}
+	render() {
+		return (
+			<Text id='pick'>{this.state.showing}</Text>
+		)
+	}
 }
   
 export default Spinner
